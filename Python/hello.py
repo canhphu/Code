5c0abd436d8a54223483edd198a9a60e954283f0
canhphu <canhphu131204@gmail.com>
2024-08-02 15:11:32 +0700

    update file

--- Python/hello.py
+++ Python/hello.py
@@ -293,3 +293,28 @@ fruits[2] = "Strawberry"
 print(fruits)
 
 print(ord('a')) #tra ve gia tri ASCII cua ki tu
+
+#OOP in Python
+class PartyAnimal:
+    x = 0
+    name = ''
+    def __init__(self,nam):
+        self.name = nam
+        print(self.name,'constructed')
+    def party(self):
+        self.x = self.x + 1
+        print(self.name, 'party count',self.x)
+class FootballFan(PartyAnimal):
+    points = 0
+    def touchdown(self):
+        self.points = self.points + 7
+        self.party()
+        print(self.name, 'points',self.points)
+q = PartyAnimal('Quincy')
+m = PartyAnimal('Miya')
+q.party()
+m.party()
+q.party()
+j = FootballFan('Jim')
+j.party()
+j.touchdown()
